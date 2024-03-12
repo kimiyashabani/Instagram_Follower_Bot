30dcdf633d488e3f14e644970c4502b5c79fec9c
kimiya shabani <kimiyashabani1378@gmail.com>
2024-03-12 20:12:30 +0100

    Changing username and password.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from InstaFollower import InstaFollower
 
 similar_account = "pycodebr"
-USERNAME = "kimiyashabani@outlook.com"
-PASSWORD = "JErU&.)z27dLjJD"
+USERNAME = "Your email comes here"
+PASSWORD = "You password comes here"
 bot = InstaFollower()
 bot.login(USERNAME,PASSWORD)
 bot.find_followers(similar_account)
